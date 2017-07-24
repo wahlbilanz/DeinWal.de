c76c306f30aca1e4a207e26b2aa7c4dfaa67f1b2
Martin Scharm <martin@binfalse.de>
2017-07-24 19:06:27 +0200

    continued and improved FAQ

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -12,4 +12,47 @@ export class FaqComponent implements OnInit {
   ngOnInit() {
   }
 
+  
+  /**
+   * toggle visibility of an FAQ content element and toggles caret if defined.
+   */
+  toggleVis(content, caret) {
+    if (content.style.display === '') {
+      content.style.display = 'none';
+      if (caret) {
+        caret.className = 'fa fa-caret-down';
+      }
+    } else {
+      content.style.display = '';
+      if (caret) {
+        caret.className = 'fa fa-caret-up';
+      }
+    }
+  }
+  
+  
+  /**
+   * hacks easy to use according function...
+   * 
+   * this can easily be used from the HTML -- see there for an example ;-)
+   */
+  accordion(clickEvent) {
+    console.log(clickEvent);
+    console.log(clickEvent.target.parentElement);
+    
+    let caret = null;
+    for (let i = 0; i < clickEvent.target.childNodes.length; i++) {
+      if (clickEvent.target.childNodes[i].nodeName === 'I') {
+        caret = clickEvent.target.childNodes[i];
+      }
+    }
+    
+    const pDiv = clickEvent.target.parentElement.parentElement;
+    
+    for (let i = 0; i < pDiv.childNodes.length; i++) {
+      if (pDiv.childNodes[i].nodeName === 'DIV') {
+        this.toggleVis(pDiv.childNodes[i], caret);
+      }
+    }
+  }
 }
