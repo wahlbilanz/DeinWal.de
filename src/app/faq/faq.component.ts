ea3e9d1cb7c0556e0c5fa87bdbf049add1d50854
Martin Scharm <martin@binfalse.de>
2017-08-22 08:06:32 +0200

    reihenfolge der anderen tools wird jetzt ausgewuerfelt

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -9,8 +9,95 @@ import { Router, NavigationEnd } from '@angular/router'
 export class FaqComponent implements OnInit, AfterContentInit, AfterViewInit, AfterViewChecked, DoCheck, OnChanges {
   localStorageOn = 'gespeichert';
   
+  othertools = [];
+  
   constructor(private router: Router) {
     this.updateLocalStorage ();
+    
+    
+
+	this.othertools = this.shuffle ([
+		/*{
+		'name': '',
+		'link': ''
+		},*/
+		{
+			'pre': 'in den',
+			'name': 'Wahl-O-Mat',
+			'link': 'https://www.wahl-o-mat.de/'
+		},
+		{
+			'pre': 'in die',
+			'name': 'Wahlprogramme der verschiedenen Parteien',
+			'link': 'http://www.bundestagswahl-bw.de/wahlprogramme_btwahl2017.html'
+		},
+		{
+			'pre': 'zu',
+			'name': 'abgeordnetenwatch.de',
+			'link': 'https://www.abgeordnetenwatch.de/'
+		},
+		{
+			'pre': 'zu',
+			'name': 'wahlversprechen2013',
+			'link': 'http://www.wahlversprechen2013.de/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Wahl.Daten.Helfer',
+			'link': 'http://www.wahldatenhelfer.de/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Steuer-O-Mat',
+			'link': 'http://steuer-o-mat.de/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Musik-O-Mat',
+			'link': 'http://musik-o-mat.com/'
+		},
+		{
+			'pre': 'zur',
+			'name': 'Wahlorientierungshilfe',
+			'link': 'https://wahl2017.gerritalex.de/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'WahlSwiper',
+			'link': 'https://movact.de/wahlswiper/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Sozial-O-Mat',
+			'link': 'https://www.sozial-o-mat.de/btw17/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Digital-Thesen-Check',
+			'link': 'https://digital17.d-64.org/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Wahlkompass Digitales',
+			'link': 'http://wahlkompass-digitales.de/'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Agrar-O-Mat',
+			'link': 'https://www.agrarheute.com/agraromat'
+		},
+		{
+			'pre': 'in die',
+			'name': 'Übersicher aller zur Wahl stehenden Parteien',
+			'link': 'https://www.bundeswahlleiter.de/info/presse/mitteilungen/bundestagswahl-2017/05_17_parteien_teilnahme.html'
+		},
+		{
+			'pre': 'zum',
+			'name': 'Europapolitischen Wahlkompass',
+			'link': 'http://www.foederalist.eu/p/europapolitischer-wahlkompass-2017.html'
+		}
+	]);
+	
   }
 
   ngOnInit() {
@@ -39,7 +126,25 @@ export class FaqComponent implements OnInit, AfterContentInit, AfterViewInit, Af
   ngOnChanges(changes) {
     this.updateLocalStorage ();
   }
-  
+
+	shuffle (array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
   
   
   updateLocalStorage () {
